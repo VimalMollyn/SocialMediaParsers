bc27907d37852e6507ca963b6b13547a1a7e6b01
Vimal Mollyn <ms123vimal@gmail.com>
2023-02-27 11:03:48 -0500

    Added parser to parse fpaths for stories and posts along with timestamps

--- instagram.py
+++ instagram.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import pandas as pd
 from datetime import datetime
 
-def parse_messages(path):
-    path_to_messages = path / "messages/inbox"
+def parse_messages(root_path):
+    path_to_messages = root_path / "messages/inbox"
 
     dfs = []
     # iterate through the inbox
@@ -16,3 +16,29 @@ def parse_messages(path):
 
     df = pd.concat(dfs, axis=0).reset_index(drop=True)
     return df
+
+def get_stories_paths(root_path):
+    path_to_stories = root_path / "media/stories"
+    paths = []
+    for fpath in path_to_stories.iterdir():
+        date = fpath.name
+        for story_path in fpath.iterdir():
+            paths.append(str(story_path))
+
+    paths_df = pd.DataFrame(paths, columns=["fpath"])
+    paths_df["date_str"] = paths_df["fpath"].str.split("/").str[-2]
+    paths_df["timestamp"] = paths_df["date_str"].apply(lambda x: datetime.strptime(x, "%Y%m").timestamp())
+    return paths_df
+
+def get_posts_paths(root_path):
+    path_to_stories = root_path / "media/posts"
+    paths = []
+    for fpath in path_to_stories.iterdir():
+        date = fpath.name
+        for story_path in fpath.iterdir():
+            paths.append(str(story_path))
+
+    paths_df = pd.DataFrame(paths, columns=["fpath"])
+    paths_df["date_str"] = paths_df["fpath"].str.split("/").str[-2]
+    paths_df["timestamp"] = paths_df["date_str"].apply(lambda x: datetime.strptime(x, "%Y%m").timestamp())
+    return paths_df
